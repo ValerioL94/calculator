fd8b4b8f0b81e205a7d1e89d6ba73799a82b3054
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2023-10-22 10:12:14 +0200

    Edit calculator layout and script functions
    -Prevented long numbers from overflowing the display box by setting
    overflow to auto.
    -Operations are now concatenated correctly: the result of the first
    operation will be the first number of the following one.
    -Both decimal point and operators can only be inserted if conditions
    are met, e.g. you cannot insert another operator/decimal point if
    there's one already.
    CE (clear entry) button only clears the last entry displayed on screen
    and the corresponding variable;
    AC (all clear) button instead resets both display and variables to
    the original state.

--- script.js
+++ script.js
@@ -3,24 +3,52 @@ const numbers = document.querySelectorAll(".num");
 const operators = document.querySelectorAll(".operator");
 const clearAll = document.getElementById("empty");
 const undo = document.getElementById("undo");
-const result = document.getElementById("equal")
+const equals = document.getElementById("equal");
+const decimal = document.getElementById("decimal");
+
+let firstNum = "";
+let secondNum = "";
+let operator = "";
+let result = "";
+let firstDecimal = "";
+let secondDecimal = "";
 
 numbers.forEach(number => {
     number.addEventListener("click", (e) => {
         if (operator === "") {
             firstNum += e.target.value;
-            display.textContent += number.value;
+            display.textContent += e.target.value;
         } else {
             secondNum += e.target.value;
-            display.textContent += number.value;
+            display.textContent += e.target.value;
         }
     });
 });
 
+decimal.addEventListener("click", (e) => {
+    if (firstDecimal === "" && firstNum !== "") {
+        firstNum += e.target.value;
+        display.textContent += e.target.value;
+        firstDecimal = decimal.value;
+    } else if (secondDecimal === "" && secondNum !== "") {
+        secondNum += e.target.value;
+        display.textContent += e.target.value;
+        secondDecimal = decimal.value;
+    }
+});
+
 operators.forEach(op => {
     op.addEventListener("click", (e) => {
-        operator = e.target.value;
-        display.textContent += op.value;
+        if (firstNum === "" || operator !== "" && secondNum === "") {
+            // do nothing
+        } else if (operator === "") {
+            operator = e.target.value;
+            display.textContent += e.target.value;
+        } else if (operator !== "" && secondNum !== "") {
+            operate(firstNum, operator, secondNum);
+            operator = e.target.value;
+            display.textContent += e.target.value;
+        }
     });
 });
 
@@ -29,40 +57,87 @@ clearAll.addEventListener("click", () => {
     firstNum = "";
     secondNum = "";
     operator = "";
+    firstDecimal = "";
+    secondDecimal = "";
 });
 
 undo.addEventListener("click", () => {
-    if (secondNum !== "") {
+    if (secondNum.charAt(secondNum.length - 1) === ".") {
+        secondDecimal = "";
+    } else if (secondNum !== "") {
         secondNum = secondNum.slice(0, -1);
     } else if (operator !== "") {
         operator = "";
+    } else if (firstNum.charAt(firstNum.length - 1) === ".") {
+        firstDecimal = "";
     } else {
         firstNum = firstNum.slice(0, -1);
     }
     display.textContent = display.textContent.slice(0, -1);
 });
 
-result.addEventListener("click", operate);
+equals.addEventListener("click", () => {
+    if (secondNum !== "") {
+        operate(firstNum, operator, secondNum);
+    } else {
+        // do nothing
+    }
+});
 
-let firstNum = "";
-let secondNum = "";
-let operator = "";
 
-function add(a, b) { return (a + b) };
-function subtract(a, b) { return (a - b) };
-function multiply(a, b) { return (a * b) };
+
+function add(a, b) {
+    result = Math.round((a + b) * 100) / 100;
+    display.textContent = result;
+    firstNum = result.toString();
+    operator = "";
+    secondNum = "";
+    firstDecimal = "";
+    secondDecimal = "";
+}
+function subtract(a, b) {
+    result = Math.round((a - b) * 100) / 100;
+    display.textContent = result;
+    firstNum = result.toString();
+    operator = "";
+    secondNum = "";
+    firstDecimal = "";
+    secondDecimal = "";
+}
+function multiply(a, b) {
+    result = Math.round((a * b) * 100) / 100;
+    display.textContent = result;
+    firstNum = result.toString();
+    operator = "";
+    secondNum = "";
+    firstDecimal = "";
+    secondDecimal = "";
+}
 function divide(a, b) {
-    if (b == 0) { return "invalid operation" }
-    else { return (a / b) }
+    if (b == 0) { display.textContent = "Invalid" }
+    else {
+        result = Math.round((a / b) * 100) / 100;
+        display.textContent = result;
+        firstNum = result.toString();
+        operator = "";
+        secondNum = "";
+        firstDecimal = "";
+        secondDecimal = "";
+    }
 };
 
 function operate(firstNum, operator, secondNum) {
-    if (operator === "+") { console.log(add(firstNum, secondNum)) }
-    else if (operator === "-") { return subtract(firstNum, secondNum) }
-    else if (operator === "*") { return multiply(firstNum, secondNum) }
-    else if (operator === "/") { return divide(firstNum, secondNum) }
-    else { return "invalid operator" }
+    switch (operator) {
+        case "+":
+            add(parseFloat(firstNum), parseFloat(secondNum));
+            break;
+        case "-":
+            subtract(parseFloat(firstNum), parseFloat(secondNum));
+            break;
+        case "*":
+            multiply(parseFloat(firstNum), parseFloat(secondNum));
+            break;
+        case "/":
+            divide(parseFloat(firstNum), parseFloat(secondNum));
+    }
 };
-
-
-
